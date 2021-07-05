e72d7ca61c88cddba594ea5e6afba2bfab232ccd
Nino-27 <jack-ken@hotmail.com>
2021-07-05 19:10:55 -0400

    conSketch

--- sketch.js
+++ sketch.js
@@ -1,3 +1,21 @@
+let imagenes = [];
+let index = 0;
+
+function preload() {
+    for (let i = 0; i < 3; i ++) {
+        imagenes[i] = loadImage(`foto_${i}.jpg`);
+    }
+
+    setInterval(cambiarFoto, 1000);
+}
+
+function cambiarFoto() {
+    index ++;
+    if (index >= imagenes.length) {
+        index = 0;
+    }
+}
+
 function setup() {
     createCanvas(400,400);
 }
@@ -5,6 +23,6 @@ function draw() {
     background(51);
 
     stroke(255);
-    
-    ellipse(30, 30, 50,50);
+
+    image(imagenes[index], 10, 10, width-20,height-20);
 }
