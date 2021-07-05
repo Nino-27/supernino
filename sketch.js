2fd779473b36b1d7be42b0a157c3ad04616e7050
Nino-27 <jack-ken@hotmail.com>
2021-07-05 19:31:23 -0400

    conSketch

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let imagenes = [];
 let index = 0;
+let bubble = [];
 
 function preload() {
     for (let i = 0; i < 3; i ++) {
@@ -16,13 +17,21 @@ function cambiarFoto() {
     }
 }
 
+function Bubble() {
+    this.x = 20;
+    this.y = 20;
+
+    this.show = function() {
+        image(imagenes[0],this.x, this.y, width-40, height-40);
+    }
+}
+
 function setup() {
     createCanvas(400,400);
+    bubble.push(new Bubble())
 }
 function draw() {
     background(51);
+    bubble[0].show();
 
-    stroke(255);
-
-    image(imagenes[index], 10, 10, width-20,height-20);
 }
