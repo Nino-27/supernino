8ac6aa616ae70c67dfe6fa8d6422cf5d7d1f6228
Nino-27 <jack-ken@hotmail.com>
2021-07-05 23:23:23 -0400

    conSketch

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
-let imagenes = [];
+//let imagenes = ["foto_0.jpg", "foto_1.jpg", "foto_2.jpg"];
 let index = 0;
 let bubble = [];
+let foto;
+let imagenes = [];
 
-/*function preload() {
-    for (let i = 0; i < 3; i ++) {
-        imagenes[i] = loadImage(`foto_${i}.jpg`);
-    }
-
-    setInterval(cambiarFoto, 1000);
-}*/
+for (let i = 0; i < 3; i ++) {
+    imagenes[i] = `foto_${i}.jpg`
+}
 
 function cambiarFoto() {
     index ++;
+
     if (index >= imagenes.length) {
         index = 0;
     }
+
+    foto = document.getElementById('imagenes').src = imagenes[index];
 }
 
 function Bubble() {
@@ -22,12 +23,18 @@ function Bubble() {
     this.y = 200;
 
     this.show = function() {
-        ellipse(this.x, this.y, 50, 50);
+        image(imagenes[index],this.x, this.y, 50, 50);
     }
 }
 
 function setup() {
     createCanvas(400,400);
+    
+
+    //imagenes = ["foto_0.jpg", "foto_1.jpg", "foto_2.jpg"]
+    let boton = createButton("Click Me");
+    boton.mousePressed(cambiarFoto);
+
     bubble.push(new Bubble())
     setInterval(cambiarFoto, 1000);
 }
